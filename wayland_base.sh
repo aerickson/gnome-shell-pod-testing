4f200870bffe95e388a794c376143c0c64544d2c
Andrew Erickson <aerickson@mozilla.com>
2023-04-11 15:46:21 -0700

    initial commit

---
+++ wayland_base.sh
@@ -0,0 +1,11 @@
+# 
+#podman run --rm --cap-add=SYS_NICE --cap-add=IPC_LOCK -ti ghcr.io/schneegans/gnome-shell-pod-33
+podman run --rm --cap-add=SYS_NICE --cap-add=IPC_LOCK -ti ghcr.io/schneegans/gnome-shell-pod-33
+
+# Start GNOME Shell.
+systemctl --user start "gnome-xsession@:99"
+
+# For example, you can run this command inside the container:
+DISPLAY=:99 gnome-control-center
+
+
---
+++ wayland_test.sh
@@ -0,0 +1,9 @@
+# Copy the framebuffer of xvfb.
+podman cp $(podman ps -q -n 1):/opt/Xvfb_screen0 .
+
+# Convert it to jpeg.
+convert xwd:Xvfb_screen0 capture.jpg
+
+# And finally display the image.
+# This way we can see that GNOME Shell is actually up and running!
+eog capture.jpg
